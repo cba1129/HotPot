eae23a11b8d1f040f89e8e2164d1a22a95d3015c
cba1129 <jenny5231726@gmail.com>
2025-02-26 23:10:02 +0800

    今天要修改 Customer.cs 的標籤(錯誤訊息要顯示中文) 註冊要修好 如果可以可能在寫好登入

--- Program.cs
+++ Program.cs
@@ -40,6 +40,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Customers}/{action=Member_Login}/{id?}");
+    pattern: "{controller=Customers}/{action=Index}/{id?}");
 // _Member  Register    Member_Login
 app.Run();
